0317015030740876f0602bd9b5aa435dbe2a0266
maksym-bielyshev <maksym.bielyshev@gmail.com>
2019-07-03 19:29:59 +0300

    new version

--- contracts_service/run.py
+++ contracts_service/run.py
@@ -1,19 +1,10 @@
-from flask import Flask
+from app import create_app
 
 
-def create_app(config_filename):
-    app = Flask(__name__)
-    app.config.from_object(config_filename)
-
-    from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
-
-    from Model import db
-    db.init_app(app)
-
-    return app
+app = create_app('config')
 
 
-if __name__ == "__main__":
-    app = create_app("config")
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host=app.config['HOST'],
+            port=app.config['PORT'],
+            debug=app.config['DEBUG'])
