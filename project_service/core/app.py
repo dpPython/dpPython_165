e5e913194c990aa0d4da55f5c3d293081f646fe0
Arthur543 <arthur54342@gmail.com>
2019-07-03 18:54:22 +0300

    initial commit

--- project_service/core/app.py
+++ project_service/core/app.py
@@ -1,6 +1,7 @@
 from .config import create_app
 from .api import api_blueprint, api
 from .controller import DataHandler, ProjectsInitializer, ProjectsResources, StatusUpdater
+from .controller import ProjectsCalc, ProjectsCalcResult
 
 app = create_app()
 app.register_blueprint(api_blueprint)
@@ -10,3 +11,6 @@ api.add_resource(ProjectsResources, '/projects/<id>')
 
 api.add_resource(DataHandler, '/projects/<id>/data')
 api.add_resource(StatusUpdater, '/projects/<id>/status')
+
+api.add_resource(ProjectsCalc, '/projects/<id>/calc')
+api.add_resource(ProjectsCalcResult, '/projects/<id>/result')
