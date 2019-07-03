f9e6fcac20fa801c8facdbec2ee701d611134a85
mupastir <mupastir@gmail.com>
2019-07-03 15:26:50 +0300

    add pytest

--- authentication_service/services/api_1_0/controllers.py
+++ authentication_service/services/api_1_0/controllers.py
@@ -6,46 +6,52 @@ from marshmallow import Schema, fields
 TTL = 900 # 15 min for session
 
 
-class SessionIdSchema(Schema):
-    sid = fields.UUID()
-
-
-class SessionSchema(Schema):
+class SessionSchemaPost(Schema):
     status = fields.Str()
     sid = fields.UUID()
 
 
+class UserSchema(Schema):
+    id = fields.UUID()
+    username = fields.Srt()
+    email = fields.Str()
+    password_hash = fileds.Str()
+    user_address = fields.Str()
+    create_user_date = fields.DateTime()
+
+
 class SessionDetails:
 
-    def __init__(self, username, user_id, sid):
+    def __init__(self, status, sid):
+        self.status = status
         self.sid = sid
-        self.username = username
-        self.user_id = user_id
-        self.create_time = datetime.now()
 
 
 class LoginController:
 
     def login(username, user_id):
+        schema_send = SessionSchemaPost()
+        user_schema = UserSchema()
+        user = user_schema.loads(user_id)
         if user_id != 'not_found':
             redis_client = current_app.extensions['redis']
             sid = uuid4()
-            session_details = SessionDetails(username=username, user_id=user_id, sid=sid)
-            schema = SessionSchema()
-            result = schema.dump(session_details)
-            redis_client.set(str(sid), result, ex=TTL)
-            return {'status': 'success', 'session_id': sid}
-        return {'status': 'failed', 'session_id': None}
+            session_details_redis = str(sid) + ',' + username + ',' + str(user_id) + ',' + str(datetime.now())
+            redis_client.set(str(sid), session_details_redis, ex=TTL)
+            send_session_details = SessionDetails('success', sid)
+            result_send = schema_send.dumps(send_session_details)
+            return result_send
+        send_session_details = SessionDetails('failed', None)
+        result_send = schema_send.dumps(send_session_details)
+        return result_send
 
 
 class SessionDetailsController:
 
     def get_details(sid):
-        schema = SessionIdSchema()
-        sid_ser = schema.load(sid)
         redis_client = current_app.extensions['redis']
-        session_details = redis_client.get(str(sid_ser))
+        session_details = redis_client.get(str(sid))
         if session_details:
             return {'status': 'logged in'}
-        return {'status': 'failed', 'desc': 'session doesn\'t exists'}
+        return {'status': 'failed', 'desc': 'session doesn\'t exists', 'sid': sid}
 
