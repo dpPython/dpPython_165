08fc91490dbe3e11dddcd62137855167b9014025
neromaru <near.3dsmax@mgail.com>
2019-07-04 17:54:34 +0300

    setting working celery

--- etl_service/app/celery_proj/celery.py
+++ etl_service/app/celery_proj/celery.py
@@ -1,8 +1,6 @@
-from celery import Celery
+from celery import Celery, Task
 
-celery = Celery('app.celery_proj',
-                broker='pyamqp://guest@192.168.99.102:32769')
-
-celery.config_from_object('app.celery_proj.celeryconfig')
+celery = Celery('app.celery_proj.celery',broker='pyamqp://guest:guest@192.168.99.102:32793', backend='amqp://guest:guest@192.168.99.102:32793')
+	
+#celery.config_from_object('app.celery_proj.celeryconfig')
 celery.autodiscover_tasks(['app.celery_proj.tasks'])
-
--- etl_service/app/celery_proj/tasks.py
+++ etl_service/app/celery_proj/tasks.py
@@ -1,12 +1,16 @@
-from .celery import celery
 from celery import chain, group
-from flask import current_app
 
 from ..controllers import create_chunk
+from ..services.services_senders import SessionService, ProjectService
+from .celery import celery
+
+sessions = SessionService()
+projects = ProjectService()
 
 @celery.task
 def send_status(uuid, status):
-    return current_app.projects.put(uuid, status)
+    #return projects.put(uuid, status)
+	pass
 
 
 @celery.task
@@ -14,21 +18,19 @@ def send_chunks(uuid, chunk_size, upload_file, parser_name):
     chunk_group = []
     for chunk in create_chunk(upload_file, parser_name, chunk_size):
         chunk_group.append(send_chunk.delay(uuid, chunk))
-    return group(chunk_group)
+    return chunk_group
 
 
 @celery.task
 def send_chunk(uuid, chunk):
-    return current_app.projects.post(uuid, chunk)
+    #return sessions.post(uuid, chunk)
+	pass
 
 
 @celery.task
 def transfer_data(project_id, chnk_size, upload_file,parser_name):
-    started = send_status.s(project_id, "In progress")
-    ended = send_status.s(project_id, "ended")
-    chunk_group = send_chunks.delay(project_id, chnk_size,
+    workflow = chain(send_status.si(project_id, "In progress"), group(send_chunks(project_id, chnk_size,
                                     upload_file,
-                                    parser_name)
-    workflow = chain(started, chunk_group, ended)
+                                    parser_name)), send_status.si(project_id, "ended"))()
     result = workflow.get()
     return None
