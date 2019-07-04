b56dbdce30d79c8e09c1e5f2afc438322ce8b15f
maksym-bielyshev <maksym.bielyshev@gmail.com>
2019-07-04 17:00:03 +0300

    test added

---
+++ contracts_service/test_data.py
@@ -0,0 +1,77 @@
+all_contracts = {
+    "Contracts": [
+        {
+            "contract_name": "Contract_1",
+            "information": "message",
+            "url": "http://localhost:5000/contracts/86c22578-e914-43f8-9125-032bd7a4419a",
+            "rule": {
+                "f_operand": "field_1",
+                "operator": "+",
+                "url": "http://localhost:5000/contracts/rules/b45de920-c55f-4782-8a7a-cdf72fe438f6",
+                "coefficient": "coef",
+                "name": "Rule_1",
+                "creation_date": "2019-07-03T19:55:35.508901+00:00",
+                "s_operand": "field_2",
+                "id": "b45de920-c55f-4782-8a7a-cdf72fe438f6"
+            },
+            "creation_date": "2019-07-03T19:55:44.693399+00:00",
+            "id": "86c22578-e914-43f8-9125-032bd7a4419a"
+        },
+        {
+            "contract_name": "Contract_3",
+            "information": "message",
+            "url": "http://localhost:5000/contracts/bd6e148d-254e-465f-b7bb-5fc7231c358a",
+            "rule": {
+                "f_operand": "field_1",
+                "operator": "+",
+                "url": "http://localhost:5000/contracts/rules/b45de920-c55f-4782-8a7a-cdf72fe438f6",
+                "coefficient": "coef",
+                "name": "Rule_1",
+                "creation_date": "2019-07-03T19:55:35.508901+00:00",
+                "s_operand": "field_2",
+                "id": "b45de920-c55f-4782-8a7a-cdf72fe438f6"
+            },
+            "creation_date": "2019-07-03T19:56:58.445296+00:00",
+            "id": "bd6e148d-254e-465f-b7bb-5fc7231c358a"
+        },
+
+        {
+            "contract_name": "Contract_4",
+            "information": "message",
+            "url": "http://localhost:5000/contracts/9150ff7a-a354-4e3b-b1b7-550974dd2fb1",
+            "rule": {
+                "f_operand": "field_number",
+                "operator": "+",
+                "url": "http://localhost:5000/contracts/rules/52343fdd-0a0d-4a5d-861f-db0cdd101554",
+                "coefficient": "coef",
+                "name": "Rule_4",
+                "creation_date": "2019-07-03T19:57:29.887045+00:00",
+                "s_operand": "field_number",
+                "id": "52343fdd-0a0d-4a5d-861f-db0cdd101554"
+            },
+            "creation_date": "2019-07-03T19:57:29.887045+00:00",
+            "id": "9150ff7a-a354-4e3b-b1b7-550974dd2fb1"
+        }
+    ]
+}
+
+contract = {
+    "Contract":
+        {
+            "contract_name": "Contract_1",
+            "information": "message",
+            "url": "http://localhost:5000/contracts/86c22578-e914-43f8-9125-032bd7a4419a",
+            "rule": {
+                "f_operand": "field_1",
+                "operator": "+",
+                "url": "http://localhost:5000/contracts/rules/b45de920-c55f-4782-8a7a-cdf72fe438f6",
+                "coefficient": "coef",
+                "name": "Rule_1",
+                "creation_date": "2019-07-03T19:55:35.508901+00:00",
+                "s_operand": "field_2",
+                "id": "b45de920-c55f-4782-8a7a-cdf72fe438f6"
+            },
+            "creation_date": "2019-07-03T19:55:44.693399+00:00",
+            "id": "86c22578-e914-43f8-9125-032bd7a4419a"
+        }
+}
---
+++ contracts_service/tests.py
@@ -0,0 +1,53 @@
+import unittest
+from run import app
+from test_data import all_contracts, contract
+import json
+
+
+class FlaskTestApi(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_get_all_contracts(self):
+        contracts = self.app.get('/contracts/')
+        self.assertEqual(contracts.status_code, 200)
+
+        content = json.loads(contracts.get_data(as_text=True))
+        self.assertEqual(content, all_contracts)
+
+    def test_get_contract(self):
+        contract = self.app.get('/contracts/bd6e148d-254e-465f-b7bb-5fc7231c358a')
+        self.assertEqual(contract.status_code, 200)
+
+        content = json.loads(contract.get_data(as_text=True))
+        self.assertEqual(content, contract)
+
+    def test_get_none_contract_404(self):
+        contract = self.app.get('/contracts/none_contract')
+        self.assertEqual(contract.status_code, 404)
+
+    def test_post_contract(self):
+        post_data = {
+            "contract_name": "test_name",
+            "information": "test_message",
+            "rule": "test_rule",
+            "f_operand": "field_1",
+            "s_operand": "field_2",
+            "operator": "+",
+            "coefficient": "5.5"
+        }
+        contract = self.app.post('/contracts/', data=json.dumps(post_data), content_type='application/json')
+        self.assertEqual(contract.status_code, 200)
+        self.assertEqual(contract.content_type, 'application/json')
+
+    def test_post_none_contract(self):
+        contract = self.app.post('/contracts/', data=None)
+        self.assertEqual(contract.status_code, 404)
+
+    def test_api_wrong_address(self):
+        contract = self.app.get('/contracts/test')
+        self.assertEqual(contract.status_code, 404)
+
+
+if __name__ == "__main__":
+    unittest.main()
